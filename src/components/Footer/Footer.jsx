76f140c03e422c920a1bf0bf45f7a5a484b1e03f
M.Kristóf <drzero123456@gmail.com>
2024-03-27 20:00:27 +0100

    footer imgs added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,10 @@
+import GithubLogo from "../../assets/GithubLogo.png";
+
 export const Footer = () => {
-  return <h1>Footer goes here</h1>;
+  return (
+    <>
+      <h1>Footer goes here</h1>
+      <img src={GithubLogo} />
+    </>
+  );
 };
